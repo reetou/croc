8b32ed797b9da0aab203971851708b9e9d2f21f7
reetou <fillmasterson@gmail.com>
2019-12-03 16:52:07 +0300

    Add get_user method

--- lib/croc/accounts.ex
+++ lib/croc/accounts.ex
@@ -20,6 +20,8 @@ defmodule Croc.Accounts do
   """
   @spec get_user!(integer) :: User.t() | no_return
   def get_user!(id), do: Repo.get!(User, id)
+  @spec get_user(integer) :: User.t() | nil
+  def get_user(id), do: Repo.get(User, id)
 
   @doc """
   Gets a user based on the params.
