35c053bea6f415d13d0a68beab55b034e9f6dd03
reetou <fillmasterson@gmail.com>
2019-12-03 16:55:35 +0300

    Add new deps

--- mix.exs
+++ mix.exs
@@ -45,7 +45,9 @@ defmodule Croc.MixProject do
       {:phauxth, "~> 2.3"},
       {:argon2_elixir, "~> 2.0"},
       {:bamboo, "~> 1.3"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:react_phoenix, "~> 1.1.0"},
+      {:memento, "~> 0.3.1"},
     ]
   end
 
