93ed17ebb50c9c1d2343dd834a84e6f18837a95f
reetou <fillmasterson@gmail.com>
2019-12-08 05:21:28 +0300

    Refactor lobby
    Make it a genserver

--- lib/croc/games/monopoly/lobby.ex
+++ lib/croc/games/monopoly/lobby.ex
@@ -1,81 +1,99 @@
 defmodule Croc.Games.Monopoly.Lobby do
-  alias Croc.Games.Monopoly.{Player}
+  alias Croc.Games.Monopoly.{Player, Card}
   alias Croc.Games.Monopoly.Lobby.Player, as: LobbyPlayer
+  alias Croc.Games.Lobby.Supervisor
   require Logger
+  use GenServer
 
-  use Memento.Table,
-    attributes: [
-      :lobby_id,
-      :options
-    ],
-    type: :ordered_set,
-    autoincrement: true
+  defstruct [
+    :lobby_id,
+    :players,
+    :options
+  ]
+
+  @registry Croc.Games.Registry.Lobby
+
+  @impl true
+  def init(%{ lobby: lobby } = state) do
+    name = lobby.lobby_id
+    {:ok, _pid} = Registry.register(@registry, name, state)
+    {:ok, %{ lobby: lobby }}
+  end
+
+  @impl true
+  def handle_call({:join, player_id}, from, %{ lobby: lobby } = state) do
+    with false <- LobbyPlayer.already_in_lobby?(player_id) do
+      {:ok, player} = Memento.transaction(fn ->
+        player = %LobbyPlayer{player_id: player_id, lobby_id: lobby.lobby_id}
+        Memento.Query.write(player)
+      end)
+      updated_lobby = Map.put(lobby, :players, lobby.players ++ [player])
+      updated_state = Map.put(state, :lobby, updated_lobby)
+      {:reply, {:ok, updated_lobby}, updated_state}
+    else
+      e -> {:reply, {:error, :already_in_lobby}, state}
+    end
+  end
+
+  @impl true
+  def handle_call({:leave, player_id}, from, %{ lobby: lobby } = state) do
+    with true <- LobbyPlayer.in_lobby?(player_id, lobby) do
+      updated_lobby = Map.put(lobby, :players, Enum.filter(lobby.players, fn p -> p.player_id != player_id end))
+      updated_state = Map.put(state, :lobby, updated_lobby)
+      {:reply, {:ok, updated_lobby}, updated_state}
+    else
+      _ -> {:reply, {:error, :not_in_lobby}, state}
+    end
+  end
+
+  @impl true
+  def handle_call({:get}, from, %{ lobby: lobby } = state) do
+    {:reply, {:ok, lobby}, state}
+  end
 
   def create(player_id, options) do
     with false <- LobbyPlayer.already_in_lobby?(player_id) do
-      {:ok, lobby} =
-        Memento.transaction(fn ->
-          players = Memento.Query.select(LobbyPlayer, {:==, :player_id, player_id})
-
-          %__MODULE__{lobby_id: lobby_id} =
-            lobby =
-            Memento.Query.write(%__MODULE__{
-              options: []
-            })
-
-          Memento.Query.write(LobbyPlayer.new(player_id, lobby_id))
-          Logger.debug("Creating new lobby #{lobby_id} and adding player in it...")
-          lobby
-        end)
+      lobby = %__MODULE__{
+        lobby_id: Ecto.UUID.generate,
+        players: [],
+        options: options
+      }
+      {:ok, lobby} = Supervisor.create_lobby_process(lobby.lobby_id, %{ lobby: lobby })
+      {:ok, %__MODULE__{}} = join(lobby.lobby_id, player_id)
     else
-      _ ->
-        Logger.error("Already in lobby, cannot create new lobby")
-        {:error, :already_in_lobby}
+      _ -> {:error, :already_in_lobby}
     end
   end
 
   def join(lobby_id, player_id) do
-    with false <- LobbyPlayer.already_in_lobby?(player_id) do
-      {:ok, lobby} = Memento.transaction(fn ->
-        lobby = Memento.Query.read(__MODULE__, lobby_id)
-
-        unless lobby == nil do
-          LobbyPlayer.create(player_id, lobby_id)
-        end
-      end)
-
-      :ok
+    with {:ok, lobby, pid} <- get(lobby_id) do
+      GenServer.call(pid, {:join, player_id})
     else
-      _ ->
-        Logger.error("Player #{player_id} is already in lobby, cannot join")
-        {:error, :already_in_lobby}
+      e -> e
     end
   end
 
   def leave(lobby_id, player_id) do
-    with true <- LobbyPlayer.already_in_lobby?(player_id),
-         {:ok, %LobbyPlayer{} = lobby_player} <- LobbyPlayer.get(player_id, lobby_id) do
-      {:ok, true} = Memento.transaction(fn ->
-        Memento.Query.delete(LobbyPlayer, lobby_player.id)
-        players_left = Memento.Query.select(LobbyPlayer, {:==, :lobby_id, lobby_id})
-        unless length(players_left) > 0 do
-          Memento.Query.delete(__MODULE__, lobby_id)
-        end
-        true
-      end)
-
-      :ok
+    with {:ok, lobby, pid} <- get(lobby_id) do
+      GenServer.call(pid, {:leave, player_id})
     else
-      _ ->
-        Logger.error("Player #{player_id} is not in lobby, cannot leave")
-        {:error, :not_in_lobby}
+      e -> e
     end
   end
 
   def get(lobby_id) when lobby_id != nil do
-    Memento.transaction(fn ->
-      Memento.Query.read(__MODULE__, lobby_id)
-    end)
+    Registry.lookup(@registry, lobby_id)
+    |> case do
+        [] -> {:error, :no_lobby}
+        processes ->
+          {pid, init_lobby} = List.first(processes)
+          with {:ok, %__MODULE__{} = lobby} <- GenServer.call(pid, {:get}, 5000) do
+            {:ok, lobby, pid}
+          else
+            e -> e
+            |> IO.inspect(label: "Probably a error at get by lobby id")
+          end
+       end
   end
 
   def has_lobby?(lobby_id) when lobby_id == nil, do: false
@@ -88,8 +106,8 @@ defmodule Croc.Games.Monopoly.Lobby do
   def has_players?(lobby_id) when lobby_id == nil, do: false
 
   def has_players?(lobby_id) do
-    with {:ok, lobby} when lobby != nil <- get(lobby_id) do
-      {:ok, players} = get_players(lobby_id)
+    with {:ok, lobby, pid} when lobby != nil <- get(lobby_id) do
+      players = lobby.players
 
       # Если игроков 2 и более, значит лобби может стартовать
       length(players) > 1
@@ -105,8 +123,7 @@ defmodule Croc.Games.Monopoly.Lobby do
   end
 
   def get_players(lobby_id) do
-    Memento.transaction(fn ->
-      Memento.Query.select(LobbyPlayer, {:==, :lobby_id, lobby_id})
-    end)
+    {:ok, lobby, pid} = get(lobby_id)
+    {:ok, lobby.players}
   end
 end
