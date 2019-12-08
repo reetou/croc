8c5818a524bc4ab2063590e3cb5e320a1f3e3f78
reetou <fillmasterson@gmail.com>
2019-12-08 05:21:40 +0300

    Refactor monopoly
    Make it a genserver

--- lib/croc/games/monopoly.ex
+++ lib/croc/games/monopoly.ex
@@ -7,8 +7,12 @@ defmodule Croc.Games.Monopoly do
 
   alias Croc.Games.Monopoly.Lobby.Player, as: LobbyPlayer
   alias Croc.Repo.Games.Monopoly.Card, as: MonopolyCard
-
+  alias Croc.Games.Monopoly.Supervisor, as: MonopolySupervisor
+  alias Croc.Games.Lobby.Supervisor, as: LobbySupervisor
+  use GenServer
   require Logger
+
+  @registry Croc.Games.Registry.Monopoly
 
   @positions Enum.to_list(0..39)
 
@@ -19,92 +23,28 @@ defmodule Croc.Games.Monopoly do
     :started_at,
     :player_turn
   ]
-  use Memento.Table,
-    attributes: [
-      :game_id,
-      :players,
-      :started_at,
-      :winners,
-      :player_turn,
-      :cards
-    ],
-    type: :ordered_set
 
-  def positions, do: @positions
+  defstruct [
+    :game_id,
+    :players,
+    :started_at,
+    :winners,
+    :player_turn,
+    :cards
+  ]
 
-  def start(%Lobby{lobby_id: game_id, options: options}) do
-    with true <- Lobby.has_lobby?(game_id),
-         true <- Lobby.has_players?(game_id) do
-      Memento.transaction(fn ->
-        started_at = DateTime.utc_now() |> DateTime.truncate(:second)
-        {:ok, lobby_players} = Lobby.get_players(game_id)
-
-        players =
-          lobby_players
-          |> Enum.map(fn %LobbyPlayer{} = p ->
-            %Player{
-              player_id: p.player_id,
-              game_id: game_id,
-              balance: 0,
-              position: 0,
-              surrender: false
-            }
-          end)
-
-        first_player = List.first(players)
-
-        game = %__MODULE__{
-          game_id: game_id,
-          players: players,
-          started_at: started_at,
-          winners: [],
-          player_turn: first_player.player_id,
-          cards: get_default_cards
-        }
-
-        Memento.Query.write(game)
-        Enum.each(players, fn p -> Memento.Query.write(p) end)
-        game
-      end)
-    else
-      _ ->
-        Logger.error("No lobby or no players")
-        {:error, :no_players_or_lobby}
-    end
+  @impl true
+  def init(%{ game: game } = state) do
+    name = game.game_id
+    {:ok, _pid} = Registry.register(@registry, name, state)
+    {:ok, state}
   end
 
-  def roll_dice do
-    {Enum.random(1..7), Enum.random(1..7)}
-  end
-
-  def can_send_action?(game_id, player_id) when game_id == nil do
-    {:error, "Unknown game_id"}
-  end
-
-  def can_send_action?(game_id, player_id) when player_id == nil do
-    {:error, "Unknown player_id"}
-  end
-
-  def can_send_action?(game_id, player_id) do
-    Memento.transaction(fn ->
-      with %__MODULE__{} = game = Memento.Query.read(__MODULE__, game_id),
-           %Player{} = player = Enum.find(game.players, fn p -> p.player_id == player_id end) do
-        # Проверяем что игрок присутствует в игре, не сдался и сейчас его очередь ходить
-        player != nil and player.surrender != true and game.player_turn == player_id
-      else
-        _err ->
-          Logger.error("Game or player not found in Game #{game_id} and Player #{player_id}")
-          false
-      end
-    end)
-  end
-
-  def move(game_id, player_id) do
-    {x, y} = roll_dice
-    move_value = x + y
-
-    Memento.transaction(fn ->
-      %__MODULE__{} = game = Memento.Query.read(__MODULE__, game_id)
+  @impl true
+  def handle_call({:move, player_id}, _from, %{ game: game } = state) do
+    with true <- can_send_action?(game.game_id, player_id) do
+      {x, y} = roll_dice
+      move_value = x + y
       player_index = Enum.find_index(game.players, fn p -> p.player_id == player_id end)
       player = Enum.at(game.players, player_index)
       old_position = player.position
@@ -121,23 +61,126 @@ defmodule Croc.Games.Monopoly do
         |> Map.put(:position, new_position)
 
       players = List.insert_at(game.players, player_index, player)
-      game = Map.put(game, :players, players)
-      Memento.Query.write(game)
-      Memento.Query.write(player)
+      updated_game = Map.put(game, :players, players)
 
-      unless player == nil do
-        {:ok, {game, player, old_position, new_position}}
-      else
-        {:error, "Player not found in game"}
-      end
-    end)
-    |> case do
-      {:ok, {game, player, old_position, new_position}} ->
-        true
-        process_move(game, player, old_position, new_position)
+      process_move(updated_game, player, old_position, new_position)
+      {:reply, {:ok, updated_game}, Map.put(state, :game, updated_game)}
+    else
+      _ -> {:reply, %{error: :not_your_turn}, state}
+    end
+  end
 
-      {:error, message} ->
-        Logger.error(message)
+  @impl true
+  def handle_call({:put_on_loan, %{player: player, card: card}}, _from, %{ game: game } = state) do
+    with true <- can_send_action?(game.game_id, player.player_id) do
+      {:ok, updated_game, _updated_player} = Card.put_on_loan(game, player, card)
+      {:reply, {:ok, updated_game}, updated_game}
+    else
+      _ -> {:reply, %{error: :not_your_turn}, state}
+    end
+  end
+
+  @impl true
+  def handle_call({:upgrade, %{player: player, card: card}}, _from, %{ game: game } = state) do
+    with true <- can_send_action?(game.game_id, player.player_id) do
+      {:ok, updated_game, _updated_player} = Card.upgrade(game, player, card)
+      {:reply, {:ok, updated_game}, updated_game}
+    else
+      _ -> {:reply, %{error: :not_your_turn}, state}
+    end
+  end
+
+  @impl true
+  def handle_call({:downgrade, %{player: player, card: card}}, from, %{ game: game } = state) do
+    with true <- can_send_action?(game.game_id, player.player_id) do
+      {:ok, updated_game, _updated_player} = Card.upgrade(game, player, card)
+      {:reply, {:ok, updated_game}, updated_game}
+    else
+      _ -> {:reply, %{error: :not_your_turn}, state}
+    end
+  end
+
+  @impl true
+  def handle_call({:get}, from, %{ game: game } = state) do
+    {:reply, {:ok, game}, state}
+  end
+
+  def positions, do: @positions
+
+  def start(%Lobby{lobby_id: lobby_id, options: options}) do
+    with {:ok, lobby_players} when length(lobby_players) > 0 <- Lobby.get_players(lobby_id) do
+      {:ok, game} = Memento.transaction(fn ->
+        started_at = DateTime.utc_now() |> DateTime.truncate(:second)
+        game_id = Ecto.UUID.generate
+        players =
+          lobby_players
+          |> Enum.map(fn %LobbyPlayer{} = p ->
+            %Player{
+              player_id: p.player_id,
+              game_id: game_id,
+              balance: 0,
+              position: 0,
+              surrender: false
+            }
+          end)
+        Enum.each(players, fn p -> Memento.Query.write(p) end)
+
+        game = %__MODULE__{
+          game_id: game_id,
+          players: players,
+          started_at: started_at,
+          winners: [],
+          player_turn: List.first(players) |> Map.fetch!(:player_id),
+          cards: get_default_cards
+        }
+
+        {:ok, game} = MonopolySupervisor.create_game_process(game.game_id, %{ game: game })
+        LobbySupervisor.stop_lobby_process(lobby_id)
+
+        game
+      end)
+    else
+      e -> e
+           |> case do
+                {:ok, []} ->
+                  Logger.error("No lobby or no players")
+                  {:error, :no_players_in_lobby}
+                _ ->
+                  Logger.error("Unknown error")
+                  {:error, :unknown_error}
+              end
+    end
+  end
+
+  def roll_dice do
+    {Enum.random(1..6), Enum.random(1..6)}
+  end
+
+  def can_send_action?(game_id, player_id) when game_id == nil do
+    {:error, :unknown_game_id}
+  end
+
+  def can_send_action?(game_id, player_id) when player_id == nil do
+    {:error, :unknown_player_id}
+  end
+
+  def can_send_action?(game_id, player_id) do
+    with {:ok, %__MODULE__{} = game, pid} = get(game_id),
+         %Player{} = player = Enum.find(game.players, fn p -> p.player_id == player_id end) do
+      # Проверяем что игрок присутствует в игре, не сдался и сейчас его очередь ходить
+      player != nil and player.surrender != true and game.player_turn == player_id
+    else
+      _err ->
+        Logger.error("Game or player not found in Game #{game_id} and Player #{player_id}")
+        false
+    end
+  end
+
+  def move(game_id, player_id) do
+    with {:ok, game, pid} <- get(game_id) do
+      GenServer.call(pid, {:move, player_id})
+    else
+      e -> e
     end
   end
 
@@ -182,5 +225,20 @@ defmodule Croc.Games.Monopoly do
 
   def get_default_cards do
     MonopolyCard.get_default_by_positions(@positions)
+  end
+
+  def get(game_id) when game_id != nil do
+    Registry.lookup(@registry, game_id)
+    |> case do
+         [] -> {:error, :no_game}
+         processes ->
+           {pid, _init_game} = List.first(processes)
+           with {:ok, %__MODULE__{} = game} <- GenServer.call(pid, {:get}, 5000) do
+             {:ok, game, pid}
+           else
+             e -> e
+                  |> IO.inspect(label: "Probably a error at get by game id")
+           end
+       end
   end
 end
