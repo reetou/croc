a75bc5c07d7234b3bdafd7dd8419c0cf1fc88536
reetou <fillmasterson@gmail.com>
2019-12-21 01:52:50 +0300

    Add remove player timeout pipeline

--- lib/croc/pipelines/games/monopoly/events/remove_player_timeout.ex
+++ lib/croc/pipelines/games/monopoly/events/remove_player_timeout.ex
@@ -13,8 +13,6 @@ defmodule Croc.Pipelines.Games.Monopoly.Events.RemovePlayerTimeout do
   use Opus.Pipeline
 
   check :has_player_turn?, error_message: :no_player_turn
-  check :has_timeout_pid?, error_message: :no_timeout_pid
-  step :kill_timeout_process, if: :process_alive?
   step :reset_timeout_data
 
 
@@ -22,23 +20,9 @@ defmodule Croc.Pipelines.Games.Monopoly.Events.RemovePlayerTimeout do
     game.player_turn != nil
   end
 
-  def has_timeout_pid?(%{ game: game }) do
-    game.timeout_pid != nil
-  end
-
-  def process_alive?(%{ game: game }) do
-    Process.alive?(game.timeout_pid)
-  end
-
-  def kill_timeout_process(%{ game: game } = args) do
-    Process.exit(game.timeout_pid, :kill)
-    args
-  end
-
   def reset_timeout_data(%{ game: game } = args) do
     game =
       game
-      |> Map.put(:timeout_pid, nil)
       |> Map.put(:on_timeout, nil)
     args
     |> Map.put(:game, game)
