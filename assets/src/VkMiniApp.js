ff1d0e800806c9945e06e475edf2f2cfb1e0c1b5
reetou <fillmasterson@gmail.com>
2019-12-27 17:16:24 +0300

    Add some handlers for vk mini app

--- assets/src/VkMiniApp.js
+++ assets/src/VkMiniApp.js
@@ -7,13 +7,33 @@ function VkMiniApp(props) {
   useEffect(() => {
     connect.send('VKWebAppInit')
 
-    connect.subscribe(e => {
+    const handler = (e) => {
       console.log('Received VK event', e)
-    })
+    }
+
+    connect.subscribe(handler)
+
+    return () => {
+      console.log('Unsubscribing')
+      connect.unsubscribe(handler)
+    }
   }, [])
+  const sendLogin = async () => {
+    try {
+      console.log('Send login')
+      const data = await connect.sendPromise('VKWebAppGetEmail')
+
+      // Handling received data
+      console.log('received email data', data);
+    } catch (error) {
+      console.log('Cannot get email data', error)
+      // Handling an error
+    }
+  }
   return (
     <PhoenixSocketProvider wsUrl="localhost:4000/socket" options={{ token: window.userToken }}>
       <LobbyContainer {...props}/>
+      <button onClick={sendLogin}>Войти</button>
     </PhoenixSocketProvider>
   )
 }
