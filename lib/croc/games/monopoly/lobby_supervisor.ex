ea9a4610b14cedee948ad4652cbcdd5427143163
reetou <fillmasterson@gmail.com>
2019-12-30 18:08:16 +0300

    Gracefully destroy lobby after 30 minutes of being active

--- lib/croc/games/monopoly/lobby_supervisor.ex
+++ lib/croc/games/monopoly/lobby_supervisor.ex
@@ -21,6 +21,7 @@ defmodule Croc.Games.Lobby.Supervisor do
     Logger.debug("Supervisor start child result at create lobby process #{inspect(pid)}")
     Logger.debug("Created lobby process under name #{lobby_id}")
     {:ok, _chat_pid} = ChatSupervisor.create_chat_process(lobby.chat_id, :lobby, lobby.players, lobby_id)
+    {:ok, _remover_pid} = schedule_removal(lobby_id)
     {:ok, lobby}
   end
 
@@ -29,5 +30,26 @@ defmodule Croc.Games.Lobby.Supervisor do
     :ok = DynamicSupervisor.terminate_child(__MODULE__, pid)
     :ok = ChatSupervisor.stop_chat_process(lobby.chat_id)
     :ok = CrocWeb.Endpoint.broadcast("lobby:all", "lobby_destroy", %{ lobby_id: lobby_id })
+  end
+
+  def schedule_removal(lobby_id, after_ms \\ 60000 * 30, reason \\ :lobby_timeout) do
+    {:ok, pid} = Task.start(fn ->
+      Process.sleep(after_ms)
+      with {:ok, lobby, _pid} <- Lobby.get(lobby_id) do
+        :ok = CrocWeb.Endpoint.broadcast("lobby:" <> lobby_id, "left", %{
+          force: true,
+          lobby_id: lobby_id,
+          reason: reason
+        })
+        Enum.each(lobby.players, fn p ->
+          Process.sleep(50)
+          {:ok, %Lobby{}} = Lobby.leave(lobby_id, p.player_id)
+        end)
+        Process.sleep(100)
+        :ok = stop_lobby_process(lobby_id)
+      else
+        _ -> Logger.debug("Lobby #{lobby_id} was not removed because already destroyed")
+      end
+    end)
   end
 end
