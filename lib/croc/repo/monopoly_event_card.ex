8e363e483eb164f364616596b4dda0df86ae35f7
reetou <fillmasterson@gmail.com>
2019-12-31 00:59:00 +0300

    Add schemas for monopoly event card

---
+++ lib/croc/repo/monopoly_event_card.ex
@@ -0,0 +1,41 @@
+defmodule Croc.Repo.Games.Monopoly.EventCard do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+  import Ecto.Query
+
+  alias Croc.Repo
+
+  schema "monopoly_event_cards" do
+    field :name, :string, null: false
+    field :description, :string, null: false
+    field :type, Ecto.Atom, null: false
+    field :disabled, :boolean, default: false, null: false
+    field :rarity, :integer, default: 0, null: false
+    field :image_url, :string
+  end
+
+  def changeset(%__MODULE__{} = event_card, attrs) do
+    event_card
+    |> cast(attrs, [:name, :description, :type, :disabled, :rarity, :image_url])
+    |> validate_required([
+      :name,
+      :description,
+      :type,
+      :rarity,
+      :image_url,
+    ])
+  end
+
+  def get_by_id(id) do
+    __MODULE__
+    |> Repo.get(id)
+  end
+
+  def get_all() do
+    __MODULE__
+    |> Repo.all()
+    |> Enum.map(fn c -> Map.from_struct(c) end)
+  end
+
+end
