a0a81c6bc84de7e371d8e801fa24d30762445e12
reetou <fillmasterson@gmail.com>
2020-01-02 01:56:48 +0300

    Change relation name

--- lib/croc/accounts/monopoly_user.ex
+++ lib/croc/accounts/monopoly_user.ex
@@ -5,6 +5,6 @@ defmodule Croc.Accounts.MonopolyUser do
   def get_event_cards(player_id) do
     Accounts.get_user!(player_id)
     |> Repo.preload(:monopoly_event_cards)
-    |> Map.fetch!(:monopoly_event_cards)
+    |> Map.fetch!(:user_monopoly_event_cards)
   end
 end
