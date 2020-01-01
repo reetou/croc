ff8f45a47ddda5ceacaf17ad0e137cfe9369bac1
reetou <fillmasterson@gmail.com>
2020-01-01 19:47:02 +0300

    Add monopoly user context

---
+++ lib/croc/accounts/monopoly_user.ex
@@ -0,0 +1,10 @@
+defmodule Croc.Accounts.MonopolyUser do
+  alias Croc.Accounts
+  alias Croc.Repo
+
+  def get_event_cards(player_id) do
+    Accounts.get_user!(player_id)
+    |> Repo.preload(:monopoly_event_cards)
+    |> Map.fetch!(:monopoly_event_cards)
+  end
+end
