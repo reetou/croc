28debb41fe23e3a5569e00d4759c9922709f8470
reetou <fillmasterson@gmail.com>
2020-01-07 07:23:07 +0300

    Add connecting to admin channel via token

--- lib/croc_web/channels/admin_channel.ex
+++ lib/croc_web/channels/admin_channel.ex
@@ -6,7 +6,27 @@ defmodule CrocWeb.AdminChannel do
 
   @prefix "admin:"
 
-  def join(@prefix <> "game_messages" = topic, _message, socket) do
+  def join(@prefix <> "game_messages" = topic, %{ "token" => token }, socket) when token != nil and token != "" do
+    case Phoenix.Token.verify(socket, "user socket", token, max_age: 1209600) do
+      {:ok, user_id} ->
+        with %User{} = user <- Accounts.get_user(user_id),
+             true <- user.is_admin == true do
+          {:ok, socket}
+        else
+          _ ->
+            {:error, %{ reason: :unknown_admin }}
+        end
+      {:error, reason} ->
+        {:error, %{ reason: reason }}
+    end
+  end
+
+  def join(@prefix <> "game_messages" = topic, message, %{ assigns: %{ user_id: nil } }) do
+    {:error, %{ reason: :unauthorized }}
+  end
+
+  def join(@prefix <> "game_messages" = topic, message, socket) do
+    IO.inspect(message, label: "message")
 #    Logger.warn("Someone is joining to user id #{socket.assigns.user_id} in game")
 #    with true <- user_id == "#{socket.assigns.user_id}" do
 #      {:ok, %{ cool: true, user_id: user_id }, socket}
